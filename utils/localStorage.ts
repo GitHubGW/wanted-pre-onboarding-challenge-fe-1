fafb1ebcc876be1708a849fc68af8f924c6d378f
GitHubGW <kowonp@gmail.com>
2023-01-01 01:32:24 +0900

    feat: LocalStorage 함수 추가

---
+++ utils/localStorage.ts
@@ -0,0 +1,10 @@
+import { TOKEN } from "constants/auth";
+
+export const getLocalStorageItem = () => {
+  const token = localStorage.getItem(TOKEN);
+  return token;
+};
+
+export const setLocalStorageItem = (token: string) => {
+  localStorage.setItem(TOKEN, token);
+};
