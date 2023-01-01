15d85d0f65f89731e566baead1548535aaba26c0
GitHubGW <kowonp@gmail.com>
2023-01-01 19:48:58 +0900

    feat: React Query Dev tool 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
 import "styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const queryClient = new QueryClient();
-
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
